9c7dd4ee7492ab12636846b524d505f5ed5c8ab7
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2023-12-10 17:45:48 -0600

    add config, better bepinex 5-6 switching

---
+++ Options/SchoolCapacityBalancerOptions.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Wayz.CS2.SchoolCapacityBalancer;
+
+public class SchoolCapacityBalancerOptions
+{
+    public IEnumerable<SchoolOptions> Options { get; set; } = new List<SchoolOptions>
+    {
+        new SchoolOptions
+        {
+            Name = "ElementarySchool01",
+            UpkeepCost = 50000,
+            StudentCapacity = 2000
+        },
+        new SchoolOptions
+        {
+            Name = "ElementarySchool01 Extension Wing",
+            UpkeepCost = 20000,
+            StudentCapacity = 1000
+        },
+        new SchoolOptions
+        {
+            Name = "College01 Extension Wing",
+            UpkeepCost = 110000,
+            StudentCapacity = 1000
+        },
+        new SchoolOptions
+        {
+            Name = "University01",
+            UpkeepCost = 375000,
+            StudentCapacity = 2500
+        },
+        new SchoolOptions
+        {
+            Name = "University01 Extension Wing",
+            UpkeepCost = 125000,
+            StudentCapacity = 1000
+        }
+    };
+
+    public IReadOnlyDictionary<string, SchoolOptions> ToDictionary()
+    {
+        var dict = new Dictionary<string, SchoolOptions>();
+        foreach (var option in Options)
+        {
+            dict.Add(option.Name, option);
+        }
+        return dict;
+    }
+
+    public int RemoveBadEntires()
+    {
+        var length = Options.Count();
+        Options = Options.Where(x => !string.IsNullOrEmpty(x.Name) && x.UpkeepCost >= 0 && x.StudentCapacity >= 0);
+        return length - Options.Count();
+    }
+}
