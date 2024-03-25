6aacc26474085d0aa28adff5f897c00d9dfeab5c
Jacob Rice <7104854+Wayzware@users.noreply.github.com>
2024-03-25 12:40:06 -0500

    add new code

---
+++ Mod.cs
@@ -0,0 +1,40 @@
+﻿using Colossal.IO.AssetDatabase;
+using Colossal.Logging;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace SchoolCapacityBalancer;
+
+public class Mod : IMod
+{
+    public static ILog log = LogManager.GetLogger($"{nameof(SchoolCapacityBalancer)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+    public static Setting m_Setting;
+
+    public void OnLoad(UpdateSystem updateSystem)
+    {
+        log.Info(nameof(OnLoad));
+
+        if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+            log.Info($"Current mod asset at {asset.path}");
+
+        m_Setting = new Setting(this);
+        m_Setting.RegisterInOptionsUI();
+        GameManager.instance.localizationManager.AddSource("en-US", new LocaleEN(m_Setting));
+
+        AssetDatabase.global.LoadSettings(nameof(SchoolCapacityBalancer), m_Setting, new Setting(this));
+
+        updateSystem.UpdateAfter<SchoolCapacityChangerSystem>(SystemUpdatePhase.PrefabUpdate);
+        updateSystem.UpdateBefore<SchoolCapacityChangerSystem>(SystemUpdatePhase.PrefabReferences);
+    }
+
+    public void OnDispose()
+    {
+        log.Info(nameof(OnDispose));
+        if (m_Setting != null)
+        {
+            m_Setting.UnregisterInOptionsUI();
+            m_Setting = null;
+        }
+    }
+}
---
+++ SchoolCapacityChangerSystem.cs
@@ -0,0 +1,78 @@
+﻿using Game;
+using Game.Prefabs;
+using System.Collections.Generic;
+using Unity.Collections;
+using Unity.Entities;
+
+namespace SchoolCapacityBalancer;
+public partial class SchoolCapacityChangerSystem : GameSystemBase
+{
+    private Dictionary<Entity, (SchoolData, ConsumptionData)> _schoolToData = new Dictionary<Entity, (SchoolData, ConsumptionData)>();
+
+    private EntityQuery _query;
+
+    protected override void OnCreate()
+    {
+        base.OnCreate();
+
+        _query = GetEntityQuery(new EntityQueryDesc()
+        {
+            All = [
+                ComponentType.ReadWrite<SchoolData>(),
+                ComponentType.ReadWrite<ConsumptionData>()
+            ]
+        });
+
+        RequireForUpdate(_query);
+    }
+
+    protected override void OnUpdate() 
+    {
+        var schools = _query.ToEntityArray(Allocator.Temp);
+
+        foreach (var school in schools)
+        {
+            Mod.log.Info($"{school}");
+
+            SchoolData schoolData;
+            ConsumptionData serviceConsumption;
+
+            if (_schoolToData.TryGetValue(school, out var data))
+            {
+                schoolData = data.Item1;
+                serviceConsumption = data.Item2;
+            }
+            else
+            {
+                schoolData = EntityManager.GetComponentData<SchoolData>(school);
+                serviceConsumption = EntityManager.GetComponentData<ConsumptionData>(school);
+
+                _schoolToData.Add(school, (schoolData, serviceConsumption));
+            }
+
+            var educationLevel = schoolData.m_EducationLevel;
+            double scalar = educationLevel switch
+            {
+                (byte)SchoolLevel.Elementary => Mod.m_Setting.ElementarySlider,
+                (byte)SchoolLevel.HighSchool => Mod.m_Setting.HighSchoolSlider,
+                (byte)SchoolLevel.College => Mod.m_Setting.CollegeSlider,
+                (byte)SchoolLevel.University => Mod.m_Setting.UniversitySlider,
+                _ => 0
+            } / 100d;
+
+            if (scalar == 0) continue;
+
+            schoolData.m_StudentCapacity = (int)(scalar * schoolData.m_StudentCapacity);
+            EntityManager.SetComponentData<SchoolData>(school, schoolData);
+
+            if (Mod.m_Setting.ScaleUpkeepWithCapacity)
+            {
+                serviceConsumption.m_Upkeep = (int)(serviceConsumption.m_Upkeep * scalar);
+            }
+            EntityManager.SetComponentData<ConsumptionData>(school, serviceConsumption);
+        }
+
+        Enabled = false;
+    }
+}
+
